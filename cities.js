b08f7217d6667dea35d554138f2c434ed50fa030
Colin <cln.gardiner@outlook.com>
2021-11-08 15:40:32 +0000

    added event listener to filter results as user types

---
+++ cities.js
@@ -0,0 +1,82 @@
+mock = {
+  "data": [
+    {
+      "id": 213,
+      "wikiDataId": "Q1021800",
+      "name": "Leskovik",
+      "country": "Albania",
+      "countryCode": "AL",
+      "region": "Korçë District",
+      "regionCode": "KO",
+      "latitude": 40.15,
+      "longitude": 20.6,
+      "population": 2655
+    },
+    {
+      "id": 333,
+      "wikiDataId": "Q1746477",
+      "name": "Milot",
+      "country": "Albania",
+      "countryCode": "AL",
+      "region": "Lezhë District",
+      "regionCode": "LE",
+      "latitude": 41.683333333,
+      "longitude": 19.716666666,
+      "population": 2157
+    },
+    {
+      "id": 1615,
+      "wikiDataId": "Q516411",
+      "name": "Retiro",
+      "country": "Argentina",
+      "countryCode": "AR",
+      "region": "Buenos Aires",
+      "regionCode": "C",
+      "latitude": -34.594722222,
+      "longitude": -58.375833333,
+      "population": 65413
+    },
+    {
+      "id": 3608,
+      "wikiDataId": "Q5574207",
+      "name": "Gneixendorf",
+      "country": "Austria",
+      "countryCode": "AT",
+      "region": "Lower Austria",
+      "regionCode": "3",
+      "latitude": 48.43333333,
+      "longitude": 15.6,
+      "population": 1150
+    },
+    {
+      "id": 1456,
+      "wikiDataId": "Q731094",
+      "name": "Villa Lugano",
+      "country": "Argentina",
+      "countryCode": "AR",
+      "region": "Buenos Aires",
+      "regionCode": "C",
+      "latitude": -34.683333333,
+      "longitude": -58.466666666,
+      "population": 126374
+    }
+  ],
+  "links": [
+    {
+      "rel": "first",
+      "href": "/v1/geo/cities?offset=0&limit=5"
+    },
+    {
+      "rel": "next",
+      "href": "/v1/geo/cities?offset=5&limit=5"
+    },
+    {
+      "rel": "last",
+      "href": "/v1/geo/cities?offset=69625&limit=5"
+    }
+  ],
+  "metadata": {
+    "currentOffset": 0,
+    "totalCount": 69627
+  }
+}
